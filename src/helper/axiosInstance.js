6a7b4b1a9aeeca1a03817df3d559a73444e61e65
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-03 11:48:29 +0530

    update axios instance

--- src/helper/axiosInstance.js
+++ src/helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:8080/api/v1",
+	baseURL: `${import.meta.env.VITE_BACKEND_URI}/api/v1`,
 	withCredentials: true,
 	headers: { "Content-Type": "application/json" },
 });
