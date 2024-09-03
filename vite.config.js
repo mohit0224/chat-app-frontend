f68c1d7b223e7025a0351346805ee3b0bc834580
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-03 12:20:03 +0530

    remove - pwa

--- vite.config.js
+++ vite.config.js
@@ -7,35 +7,35 @@ import path from "path";
 export default defineConfig({
 	plugins: [
 		react(),
-		VitePWA({
-			registerType: "prompt",
-			injectRegister: false,
+		// VitePWA({
+		// 	registerType: "prompt",
+		// 	injectRegister: false,
 
-			pwaAssets: {
-				disabled: false,
-				config: true,
-			},
+		// 	pwaAssets: {
+		// 		disabled: false,
+		// 		config: true,
+		// 	},
 
-			manifest: {
-				name: "\u0015Chat app",
-				short_name: "Chat App",
-				description: "\u0015Chat app",
-				theme_color: "#ffffff",
-			},
+		// 	manifest: {
+		// 		name: "\u0015Chat app",
+		// 		short_name: "Chat App",
+		// 		description: "\u0015Chat app",
+		// 		theme_color: "#ffffff",
+		// 	},
 
-			workbox: {
-				globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
-				cleanupOutdatedCaches: true,
-				clientsClaim: true,
-			},
+		// 	workbox: {
+		// 		globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+		// 		cleanupOutdatedCaches: true,
+		// 		clientsClaim: true,
+		// 	},
 
-			devOptions: {
-				enabled: false,
-				navigateFallback: "index.html",
-				suppressWarnings: true,
-				type: "module",
-			},
-		}),
+		// 	devOptions: {
+		// 		enabled: false,
+		// 		navigateFallback: "index.html",
+		// 		suppressWarnings: true,
+		// 		type: "module",
+		// 	},
+		// }),
 	],
 	resolve: {
 		alias: {
