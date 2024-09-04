d04231fe9feb0143508723ba719e9e33ff47bf62
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-04 14:33:57 +0530

    check

--- src/helper/axiosInstance.js
+++ src/helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: `${import.meta.env.VITE_BACKEND_URI}/api/v1`,
+	baseURL: `/api/v1`,
 	withCredentials: true,
 	headers: { "Content-Type": "application/json" },
 });
